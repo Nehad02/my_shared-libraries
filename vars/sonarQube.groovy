69d09457971f44ab5127c39530279a2cbbc7344d
Nehad02 <nehagurav3101@gmail.com>
2026-07-16 21:50:17 +0530

    Update sonarQube.groovy

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -1,5 +1,9 @@
 def call() {
     withSonarQubeEnv('SonarCloud') {
-        sh "mvn sonar:sonar"
+        sh """
+            mvn sonar:sonar \
+            -Dsonar.projectKey=Nehad02_maven_pro \
+            -Dsonar.organization=nehad02
+        """
     }
 }
